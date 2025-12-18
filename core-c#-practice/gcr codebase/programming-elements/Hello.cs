2a5eb7b818683fe82c5c2fc48ba36c833116bc03
Akshay-368 <124948736+Akshay-368@users.noreply.github.com>
2025-12-18 12:26:20 +0530

    Modify file into Hello.cs to greet user by name

---
+++ core-c#-practice/gcr codebase/programming-elements/Hello.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Hello
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter your name : ");
+        string name = Console.ReadLine();
+        Console.WriteLine( "Hello , " + name + "  Welcome to Bridge-Labz ! ");
+    }
+}
