95237977280278421d4375b779fc4a13a0caf37d
Akshay-368 <124948736+Akshay-368@users.noreply.github.com>
2025-12-19 15:50:29 +0530

    Pushed Question 3 of l-1

---
+++ core-c#-practice/gcr codebase/c#-programming-elements/Level-1/Kmtomiles.cs
@@ -0,0 +1,16 @@
+using System;
+class Kmtomiles
+{
+    public static void Main()
+    {
+        /*3. Create a program to convert the distance of 10.8 kilometers to miles.
+Hint: 1 km = 1.6 miles
+I/P => NONE
+O/P => The distance ___ km in miles is ___
+        */
+
+        double k = 10.8;
+        double miles = k / 1.6 ;
+        Console.WriteLine( " The distance " + k + " km in miles is :  " + miles ) ;
+    }
+}
