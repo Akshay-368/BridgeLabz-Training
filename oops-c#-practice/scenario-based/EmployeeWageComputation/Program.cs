126b42952e7a19ac267dfd59187f39257a8a5b6c
Akshay-368 <124948736+Akshay-368@users.noreply.github.com>
2026-01-07 16:13:40 +0530

    Push the menu file for  Employee Wage Computation Program.cs

---
+++ oops-c#-practice/scenario-based/EmployeeWageComputation/Program.cs
@@ -0,0 +1,34 @@
+// file: Program.cs
+using System;
+
+public class progam
+{
+   public static void Main(string[] args)
+   {
+      /*
+      Employee Wage Computation Problem
+      Outcome has to be
+      1. Ability to work with c# Programming Constructs
+      2. Usage of Classes, Class Variables and Methods
+      3. Usage of Objects, Instances, Instance Variables and Methods
+      4. Usage of Collection Library
+      5. Ability to work with basic git flows.
+      Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
+      UC1 : Check Employee is Present or Absent - Use ((RANDOM)) for Attendance Check
+      UC2 : Calculate Daily Employee Wage - Assume (( Wage per Hour is 20 - Assume Full Day Hour is 8 ))
+      UC3 : Add Part time Employee & Wage - Assume Part time Hour is 8
+      UC4 : Solving using Switch Case Statement
+      UC5 : Calculating Wages for a Month - Assume 20 Working Day per Month
+      UC6 : Calculate Wages till a condition of total working hours or days is reached for a month - Assume 100 hours and 20 days
+
+      */
+
+      Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+
+      menu.show();   // call menu from other file
+
+      // waitng , for user to enter the inupt before closng
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
+   }
+}
